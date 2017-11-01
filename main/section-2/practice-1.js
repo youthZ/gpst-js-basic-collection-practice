1ffcfc46970ffdef7a3dfeb1b338f25d4a283b9c
youthZ <552099787@qq.com>
2017-11-01 15:41:57 +0800

    Update practice-1.js

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,60 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+	if(collection.length > 0){
+
+		var result=[];
+
+		var length = collection.length;
+
+		var index, indexResult = 0, indexTemp = 0;
+
+		result[0] = new Object();
+
+		result[0].key = collection[0];
+
+		result[0].count = 1;
+
+		for (index = 1; index < length ; index++)
+
+		{
+
+			while( indexTemp < result.length){
+
+				if(collection[index] == result[indexTemp].key){
+
+					result[indexTemp].count += 1;
+
+					break;
+
+				}else{
+
+					indexTemp ++;
+
+				}
+
+			}
+
+			if(indexTemp == result.length){
+
+				indexResult ++;
+
+				result[indexResult] = new Object();
+
+				result[indexResult].key = collection[index];
+
+				result[indexResult].count = 1;
+
+				indexTemp = 0;
+
+			}
+
+			
+
+		}
+
+		return result;
+
+	}
 }
+
