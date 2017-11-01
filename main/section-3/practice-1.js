ea98cc04a9573b8938d790f64962b8052dc70dfe
youthZ <552099787@qq.com>
2017-11-01 15:43:48 +0800

    Update practice-1.js

--- main/section-3/practice-1.js
+++ main/section-3/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  if(collectionA.length > 0 && typeof(objectB)=="object"){
+	 var lengthA = collectionA.length;
+	 var lengthB = objectB.value.length;
+	for(var indexA = 0; indexA < lengthA; indexA++){
+		for(var indexB = 0; indexB < lengthB; indexB++){
+			if(collectionA[indexA].key == objectB.value[indexB]){
+				collectionA[indexA].count -= 1;
+			}
+		}
+	}
+	return collectionA;
+  } 
 }
