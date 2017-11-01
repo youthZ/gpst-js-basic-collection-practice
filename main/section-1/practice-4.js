b5557ed679dcae575e8e322fcc3a3e8bc06f8220
youthZ <552099787@qq.com>
2017-11-01 15:41:19 +0800

    Update practice-4.js

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,5 +1,37 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+if(collectionA.length > 0 && typeof(objectB)=='object'){
+
+		if(objectB.value != undefined){
+
+			var lengthA = collectionA.length;
+
+			var lengthB = objectB.value.length;
+
+			var indexA, indexB, indexResult=0;
+
+			var result = [];
+
+			for(indexA = 0; indexA < lengthA; indexA++){
+
+				for(indexB = 0; indexB < lengthB; indexB++){
+
+					if(collectionA[indexA].key == objectB.value[indexB]){
+
+						result[indexResult] = collectionA[indexA].key;
+
+						indexResult++;
+
+					}
+
+				}
+
+			}
+
+			return result;
+
+		}
+
+	}
 }
