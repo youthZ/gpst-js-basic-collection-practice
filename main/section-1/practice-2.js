63a83f9dae041aaac1c03f2b7274789b9ae38b6c
youthZ <552099787@qq.com>
2017-11-04 12:39:42 +0800

    Update practice-2.js

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -5,10 +5,10 @@ module.exports = function collectSameElements(collectionA, collectionB) {
 	var result =[];
 	var lengthA = collectionA.length;
 	var lengthB_row = collectionB.length;
-	var lengthB_col = collectionB[0].length;
 	var indexA, indexB_row, indexB_col, indexResult=0;
 	for(indexA = 0; indexA < lengthA; indexA++){
 		for(indexB_row = 0; indexB_row < lengthB_row; indexB_row++){
+			var lengthB_col = collectionB[indexB_row].length;
 			for(indexB_col = 0; indexB_col < lengthB_col; indexB_col++){
 				if(collectionA[indexA] == collectionB[indexB_row][indexB_col]){
 					result[indexResult] = collectionA[indexA];
